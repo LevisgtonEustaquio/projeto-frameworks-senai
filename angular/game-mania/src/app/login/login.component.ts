6ba345dca94b0119e4f1732f6a02637fb1ea51c2
Levisgton Eustaquio <leoelopes@gmail.com>
2023-02-20 02:03:00 -0300

    adicionado tela de login, otimizar estilos de todas as paginas // estudar sobre autenticação de tela de login, e criação da tela de cadastro com hidden

--- angular/game-mania/src/app/login/login.component.ts
+++ angular/game-mania/src/app/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,29 +7,6 @@ import { RouterLink } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor(
-    private router: RouterLink
-) { }
 
-/* ngOnInit() {
-
-}
-
-goToInicio() {
-    this.router.navigate(['/login']);
-}
-
-}
-
-  login(){
-    let logado:number =0;
-    let usuario:any = document.getElementById("usuario")?.ariaValueNow;
-    let senha:any = document.getElementById("senha")?.ariaValueNow;
-
-    if(usuario == 'admin' && senha == '123456'){
-      goToInicio();
-      logado = 1;
-    }
-  } */
 }
 
