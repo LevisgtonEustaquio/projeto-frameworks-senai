9a5dfe6cdc275775dd657469edaebd816d8a061a
Levisgton Eustaquio <leoelopes@gmail.com>
2023-03-06 15:57:14 -0300

    Atividade de CRUD completa

--- angular/game-mania/src/app/login/login.component.ts
+++ angular/game-mania/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +9,17 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  public usuario!: string;
+  public senha!: string;
+
+  constructor(private _loginService: LoginService, private _router: Router){ }
+    
+    fazerLogin(){
+      this._loginService.login(this.usuario, this.senha);
+      this._router.navigate(['restrito/lista']);
+      this._loginService.setMostraMenu(false);
+    }
+ 
  
     
   }
